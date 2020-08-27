4ca327e2cbd507407685aab7a22f4e46798973c0
levifengqing <616744015@qq.com>
2020-08-27 14:00:20 +0800

    配置多环境配置文件

--- src/main/java/com/test/importdatas/staticTest/poi/GetExcelDatas.java
+++ src/main/java/com/test/importdatas/staticTest/poi/GetExcelDatas.java
@@ -1,5 +1,6 @@
 package com.test.importdatas.staticTest.poi;
 
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
@@ -13,27 +14,30 @@ import java.io.IOException;
 
 public class GetExcelDatas {
     public static void main(String[] args) throws IOException {
-        File file = new File("C:\\Users\\29097\\Desktop\\task\\cf\\20200804 刘明洋 市政养护系统修改121附表1：绿化汇总台账数据增加绿化识别码.xls");//正常查询
+        File file = new File("C:\\Users\\29097\\Desktop\\task\\sz\\通道基础数据.xls");//正常查询
         //获得该文件的输入流
         FileInputStream stream = new FileInputStream(file);
         // 多态  抛异常
 //        Workbook sheets = new XSSFWorkbook(stream);
-        XSSFWorkbook sheets = new XSSFWorkbook(stream);
+        Workbook sheets = new HSSFWorkbook(stream);
         //获取一个工作表(sheet页)，下标从0开始
         Sheet sheet = sheets.getSheetAt(0);
         for (int i = 1; i<=sheet.getLastRowNum() ; i++) {
             // 获取行数
             Row row = sheet.getRow(i);
 
-            row.getCell(5).setCellType(Cell.CELL_TYPE_STRING);
-            row.getCell(6).setCellType(Cell.CELL_TYPE_STRING);
-            row.getCell(7).setCellType(Cell.CELL_TYPE_STRING);
-            row.getCell(8).setCellType(Cell.CELL_TYPE_STRING);
-            String s1 = row.getCell(5).getStringCellValue();
-            String s2 = row.getCell(6).getStringCellValue();
-            String s3= row.getCell(7).getStringCellValue();
-            String s4= row.getCell(8).getStringCellValue();
-            String sql = "update damage set s1 = '"+s1+"',s2 = '"+s2+"' where s3 = '"+s3+"',s4 = '"+s4+"';";
+            row.getCell(0).setCellType(Cell.CELL_TYPE_STRING);
+            row.getCell(1).setCellType(Cell.CELL_TYPE_STRING);
+//            row.getCell(2).setCellType(Cell.CELL_TYPE_STRING);
+            row.getCell(10).setCellType(Cell.CELL_TYPE_STRING);
+            row.getCell(11).setCellType(Cell.CELL_TYPE_STRING);
+//            row.getCell(5).setCellType(Cell.CELL_TYPE_STRING);
+            String code = row.getCell(0).getStringCellValue();
+            String name = row.getCell(1).getStringCellValue();
+//            String start= row.getCell(2).getStringCellValue();
+            String lon= row.getCell(10).getStringCellValue();
+            String lat= row.getCell(11).getStringCellValue();
+            String sql = "insert into C_Bridge(ID,CODE,NAME,BRITYPE,YHOFFICEID,XCOFFICEID,LON,LAT,ISDELETE) values (NEWID(),'"+code+"','"+name+"','04','0003','0004','"+lon+"','"+lat+"','0');";
 
             System.out.println(sql);
 
